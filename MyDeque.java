92e0e5f2b93d3bc86f8a1ba0b6e6b986978ab073
Ben Shpetner <bshpetner00@stuy.edu>
2019-04-01 07:26:10 -0400

    made minor edits to add process and other miscellaenous changes

--- MyDeque.java
+++ MyDeque.java
@@ -21,37 +21,44 @@ public class MyDeque<E> {
 	public int size(){ 
 		return size;
 	}
-	public String toString(){ 
-		String r = "{";
-		if (size == 0) { //empty
-			return r + "}";
-		}
-		if (start <= end) { //normal conditions
-			for (int i = start; i <= end; i++) {
-				if (data[i] != null) {	
-					r = r + data[i] + " ";
+	public String toString() {
+		if (size == 0) {
+			return "{ }";
+		} 
+		String s = "{";
+		if (start <= end){
+			for (int i = start; i <= end; i++) { 
+				if (data[start] != null) {
+					s += data[i] + " ";
 				}
 			}
 		}
-		else { // start > end, needs to loop around
-			for (int j = start; j != end+1; j++) {
-				if (data[j] != null) {
-					r = r + data[j] + " ";
-				}
-				if (j == data.length-1) {
-					j = 0;
-				}
+		int j = 0;
+		while (j != end+1) {
+			s += data[j] + " ";
+			if (j == data.length-1){
+				j = 0;
+			}
+			else {
+				j++;
 			}
 		}
-		return r + "}";
+		return s + "}";
 	}
 
 	@SuppressWarnings("unchecked")
 	private void resize() {
 		E[] resizedArray = (E[])new Object[data.length*2+1];
 		int i = 0;
-		for (E s:data) {
-			resizedArray[i] = data[i];
+		int c = start;
+		while (i < data.length) {
+			resizedArray[i] = data[c];
+			if (c == data.length-1) {
+				c = 0;
+			}
+			else {
+				c++;
+			}
 			i++;
 		}
 		data = resizedArray;
@@ -62,39 +69,42 @@ public class MyDeque<E> {
 		if (element.equals(null)) {
 			throw new NullPointerException("Element is null.");
 		}
-		if (size == data.length) {
-			resize();
-			start = data.length-1;
-			end = size-1;
-		}
-		else if (size >= 0) {
+		if (size != 0) { //non-empty
 			if (start == 0) {
 				start = data.length-1;
 			}
-			else {
-				start--;
+			else if (size == data.length) {
+				resize();
+				start = data.length-1;
+				end = size-1;
 			}
+			else {
+				start-=1;
+			}
+			size++;
+			data[start] = element;
 		}
-		size++;
-		data[start] = element;
+		else {
+			data[start] = element;
+			size++;
+		}
+		
 	}
 
 	public void addLast(E element) throws NullPointerException{
 		if (element.equals(null)) {
 			throw new NullPointerException("Element is null.");
 		}
-		if (size >= 0) {
-			if (size == data.length) {
-				resize();
-				start = 0;
-				end = size;
-			}
-			else {
-				end++;
-			}
+		if (size == data.length) {
+			resize();
+			end = size;
+			start = 0;
 		}
 		else if (end == data.length-1) {
 			end = 0;
+		}
+		else if (size != 0) {
+			end++;
 		}
 		size++;
 		data[end] = element;
