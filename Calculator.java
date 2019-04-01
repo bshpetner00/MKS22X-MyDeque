bfd311684de86f7e122dfca2fb3aa1d8f01fdfad
Ben Shpetner <bshpetner00@stuy.edu>
2019-04-01 07:29:46 -0400

    added in the deque and argument arrays

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,9 @@
+import java.util.*;
+import java.io.*;
+
 public class Calculator {
 	public static double eval(String s) {
-		
+		String[] arguments = s.split(" ");
+		MyDeque<Double> deque = new MyDeque(arguments.length);
 	}
 }
