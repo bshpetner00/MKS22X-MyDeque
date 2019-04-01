a37930defcf357021f118a4d395e09cf0cec890c
bshpetner00 <bshpetner00@stuy.edu>
2019-04-01 14:30:49 -0400

    fixed variable name error that prevented compilation

--- Calculator.java
+++ Calculator.java
@@ -6,35 +6,35 @@ public class Calculator {
 		String[] arguments = s.split(" ");
 		MyDeque<Double> deque = new MyDeque(arguments.length);
 		for (int i = 0; i < arguments.length; i++) {
-			if (ary[i] == "+") {
+			if (arguments[i] == "+") {
 				double o = deque.removeLast();
 				double k = deque.removeLast();
 				deque.addLast(o+k);
 			}
-			else if (ary[i] == "-") {
+			else if (arguments[i] == "-") {
 				double o = deque.removeLast();
 				double k = deque.removeLast();
 				deque.addLast(k-o);
 			}
-			else if (ary[i] == "*") {
+			else if (arguments[i] == "*") {
 				double o = deque.removeLast();
 				double k = deque.removeLast();
 				deque.addLast(o*k);
 			}
-			else if (ary[i] == "/") {
+			else if (arguments[i] == "/") {
 				double o = deque.removeLast();
 				double k = deque.removeLast();
 				deque.addLast(k/o);
 			}
-			else if (ary[i] == "%") {
+			else if (arguments[i] == "%") {
 				double o = deque.removeLast();
 				double k = deque.removeLast();
 				deque.addLast(k%o);
 			}
 			else {
-				deque.addLast(Double.parseDouble(ary[i]));
+				deque.addLast(Double.parseDouble(arguments[i]));
 			}
 		}
-		return helper.removeLast();
+		return deque.removeLast();
 	}
 }
