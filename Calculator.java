1b947eda03fc67bfb9fdc287169724d4eb8541ed
Ben Shpetner <bshpetner00@stuy.edu>
2019-04-01 07:33:42 -0400

    created subtraction case

--- Calculator.java
+++ Calculator.java
@@ -6,10 +6,15 @@ public class Calculator {
 		String[] arguments = s.split(" ");
 		MyDeque<Double> deque = new MyDeque(arguments.length);
 		for (int i = 0; i < arguments.length; i++) {
-			if (ary[i].equals("+")) {
+			if (ary[i] == "+") {
 				double o = deque.removeLast();
 				double k = deque.removeLast();
 				deque.addLast(o+k);
+			}
+			else if (ary[i] == "-") {
+				double o = deque.removeLast();
+				double k = deque.removeLast();
+				deque.addLast(k-o);
 			}
 		}
 	}
