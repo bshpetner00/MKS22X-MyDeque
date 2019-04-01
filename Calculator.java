f9490032e81e8e77c6f94a55481f5508ebcfe968
Ben Shpetner <bshpetner00@stuy.edu>
2019-04-01 07:32:12 -0400

    created addition case

--- Calculator.java
+++ Calculator.java
@@ -5,5 +5,13 @@ public class Calculator {
 	public static double eval(String s) {
 		String[] arguments = s.split(" ");
 		MyDeque<Double> deque = new MyDeque(arguments.length);
+		for (int i = 0; i < arguments.length; i++) {
+			if (ary[i].equals("+")) {
+				double o = deque.removeLast();
+				double k = deque.removeLast();
+				deque.addLast(o+k);
+			}
+		}
 	}
+
 }
